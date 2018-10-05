b83a97bbdf5a9b056e4973696a9495ee4c2260db
Mark Rousskov <mark.simulacrum@gmail.com>
2018-10-04 18:47:23 -0600

    Change to specific versions of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
-  },
-  mode: "production"
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+    },
+    mode: "production"
 };
